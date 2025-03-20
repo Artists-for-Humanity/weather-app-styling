691ffaabfea4f9d90f24ee31bd37b628e2256fb8
nivealba <tnguyen301@bostonk12.org>
2025-03-20 15:36:32 -0400

    Revert "Make a first code"
    This reverts commit 2e3cb5edf360dd5a51c013d09bd151f0c9238425.

--- src/components/CountryDetail.jsx
+++ src/components/CountryDetail.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import CountryWeather from "./CountryWeather";
-import '../../src/App.css'
-
-
 
 const CountryDetails = ({ country }) => {
-    const { name, capital, area, languages, flags } = country;
+    const { name, capital, area, languages, flags} = country;
 
     return (
-        <div class='inf_format'>
+        <div>
+            <h2>{name.common}</h2>
+            <p><strong>Capital:</strong> {capital?.[0]}</p>
+            <p><strong>Area:</strong> {area} km²</p>
+            <h3>Languages:</h3>
+            <ul>
+                {Object.values(languages).map((language) => <li key={language}>{language}</li>)}
+            </ul>
             <img src={flags.svg} alt={`Flag of ${name.common}`}
                 style={{
                     boxShadow: "10px 10px 15px rgba(0, 0, 0, 0.1)",
-                    width: "100px", // Optional: Adjust the size
+                    width: "300px", // Optional: Adjust the size
                     height: "auto", // Optional: Maintain aspect ratio
                 }} />
-            <p><strong>{name.common}</strong> capital: {capital?.[0]}, area: {area} km², Languages: {Object.values(languages).map((language) => { language })}</p>
-            <CountryWeather capital={capital?.[0]} />
+            <CountryWeather capital={capital?.[0]}/>
         </div>
     );
 };
