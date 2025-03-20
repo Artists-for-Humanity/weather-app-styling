17ad983b480548ff47c20ecd08ccd21ddb8bdf39
Jesus <JesusManon25@gmail.com>
2025-03-20 17:46:35 -0400

    Key

--- src/services/countriesService.js
+++ src/services/countriesService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 // get weather api key from openweathermap.org then paste below
-const weatherApiKey = ''
+const weatherApiKey = '07fe70e062a68d0130681dc1e672c568'
 const baseUrl = `https://studies.cs.helsinki.fi/restcountries/api/all`
 
 const getWeatherByCityName = async (cityName) => {
